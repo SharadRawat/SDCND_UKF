e6209c8e7360b295336496b47dce98ab325a4586
Sharad Rawat <rawat.33@osu.edu>
2018-08-10 14:00:13 -0400

    Add files via upload

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -29,7 +29,7 @@ UKF::UKF() {
 
   // Process noise standard deviation yaw acceleration in rad/s^2
   std_yawdd_ = 30;
-  
+
   //DO NOT MODIFY measurement noise values below these are provided by the sensor manufacturer.
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
@@ -46,7 +46,7 @@ UKF::UKF() {
   // Radar measurement noise standard deviation radius change in m/s
   std_radrd_ = 0.3;
   //DO NOT MODIFY measurement noise values above these are provided by the sensor manufacturer.
-  
+  time_us_ = 0.0;
   /**
   TODO:
 
@@ -54,6 +54,14 @@ UKF::UKF() {
 
   Hint: one or more values initialized above might be wildly off...
   */
+  n_x_ = 5;
+  n_aug_ = 7;
+  lambda_ = 3 - n_aug_;
+  P_<< 1,0,0,0,0,
+        0,1,0,0,0,
+        0,0,1,0,0,
+        0,0,0,1,0,
+        0,0,0,0,1;
 }
 
 UKF::~UKF() {}
@@ -62,6 +70,7 @@ UKF::~UKF() {}
  * @param {MeasurementPackage} meas_package The latest measurement data of
  * either radar or laser.
  */
+
 void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   /**
   TODO:
@@ -69,6 +78,47 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
+  if (!is_initialized_){
+        //initializing x vector
+        x_<< 1,1,1,1,1;
+        time_us_ = meas_package.timestamp_;
+    if (meas_package.sensor_type_== MeasurementPackage::RADAR){
+            double rho = meas_package.raw_measurements_[0];
+            double phi = meas_package.raw_measurements_[1];
+            double rho_dot = meas_package.raw_measurements_[2];
+            double x_m = rho*cos(phi);
+            double y_m = rho*sin(phi);
+            if (x_m < 0.0001){
+                x_m = 0.0001;
+            }
+            if (y_m < 0.0001){
+                y_m = 0.0001;
+            }
+            x_(0) = x_m;
+            x_(1) = y_m;
+
+    }
+    else if (meas_package.sensor_type_== MeasurementPackage::RADAR){
+            x_(0) = meas_package.raw_measurements_[0];
+            x_(1) = meas_package.raw_measurements_[1];
+
+    }
+      is_initialized_ = true;
+  return;
+  }
+  float delta_t = (meas_package.timestamp_ - time_us_ )/1000000;
+  time_us_ = meas_package.timestamp_;
+
+  Prediction(delta_t);
+
+  if (meas_package.SensorType == MeasurementPackage::LASER){
+    UpdateLidar(meas_package);
+
+    }
+
+    if (meas_package.SensorType == MeasurementPackage::RADAR){
+    UpdateRadar(meas_package);
+
 }
 
 /**
@@ -79,10 +129,51 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
 void UKF::Prediction(double delta_t) {
   /**
   TODO:
-
   Complete this function! Estimate the object's location. Modify the state
   vector, x_. Predict sigma points, the state, and the state covariance matrix.
   */
+  /**** Generating Sigma Points
+  MatrixXd Xsig = MatrixXd(n_x_, 2*n_x_+1);
+  MatrixXd A = P.llt().matrixL();
+
+  Xsig.cols(0) = x_;
+  for (double i=0;i<2*n_x_+1;++i){
+    Xsig.cols(i+1) =  x + sqrt(lambda_+ n_x_)*A.cols(i);
+    Xsig.cols(i+1 + n_x_) = x - sqrt(lambda_+ n_x_)*A.cols(i);
+  }*/
+
+  /****** Generating Aug sigma Points *******/
+    MatrixXd Xsig_aug = MatrixXd::Zero(n_aug_, 2*n_aug_+1);
+    MatrixXd A = P.llt().matrixL();
+    VectorXd x_aug_ = VectorXd::Zero(7);
+    x_aug_.head(n_x_) = x_;
+
+    MatrixXd Q_ = MatrixXd:Zero(2,2);
+    Q_ << std_a_*std_a_,0,0,std_yawdd_*std_yawdd_;
+    MatrixXd P_aug_ = MatrixXd:Zero(n_aug_,n_aug_);
+    P_aug_.topLeftCorner(n_x_,n_x_) = P_;
+    P_aug_.bottomRightcorner((n_aug_-n_x_),(n_aug_-n_x_)) = Q;
+    MatrixXd L = P_aug_.llt.()matrixL;
+    Xsig.cols(0) = x__aug;
+    for (double i=0;i<2*n_aug_+1;++i){
+        Xsig_aug.cols(i+1) =  x_aug_ + sqrt(lambda_+ n_aug_)*L.cols(i);
+        Xsig_aug.cols(i+1 + n_aug_) = x_aug_ - sqrt(lambda_+ n_aug_)*L.cols(i);
+  }
+   /* Predicting sigma points */
+
+   ig_pred_ = Matrix(n_x_,2*n_aug_ + 1);
+   for (double i=0;i<2*n_aug_+1;++i){
+    double px   = Xsig_aug(0,i);
+    double py   = Xsig_aug(1,i);
+    double v    = Xsig_aug(2,i);
+    double yaw  = Xsig_aug(3,i);
+    double yawd = Xsig_aug(4,i);
+    double nu_a = Xsig_aug(5,i);
+    double nu_yawdd = Xsig_aug(6,i);
+
+    Xsig_pred_
+   }
+
 }
 
 /**
